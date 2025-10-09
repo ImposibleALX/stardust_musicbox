438315d9d91dd6ea003ee5a094b3ab34e25fa629
ImposibleALX <grupo_ept2024@hotmail.com>
2025-10-08 21:55:25 -0500

    add

--- js/expanse.js
+++
@@ -1,6 +0,0 @@
-// Solo fetch y traspaso del catálogo a main.js
-fetch('../assets/data/music_catalog_expanse.json')
-    .then(res => res.json())
-    .then(data => {
-        window.setCatalog(data);
-    });
--- js/n.js
+++
@@ -1,6 +0,0 @@
-// Solo fetch y traspaso del catálogo a main.js
-fetch('../assets/data/music_catalog_n.json')
-    .then(res => res.json())
-    .then(data => {
-        window.setCatalog(data);
-    });
